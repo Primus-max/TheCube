74422b8542b1201d434dc3d0e7e8e54b94b431e0
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-24 16:41:24 +0300

    Добавьте файлы проекта.

---
+++ TheCube.cpp
@@ -0,0 +1,21 @@
+﻿// Написать функцию, которая возвращает куб числа.
+//
+
+#include <iostream>
+#include <windows.h>
+using namespace std;
+#define CUBE(x) ((x) * (x) * (x))
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int cubNum = 0;
+    cout << "Введите число и узнаете каково оно в кубе : ";
+    cin >> cubNum;
+    
+    cout <<  " Куб числа :" << CUBE(cubNum) << endl;
+}
+
